3f48f26819ed12eaf93dfc3f5179ba27c55bf007
kirill <ischencko.kir@gmail.com>
2023-09-08 09:54:59 +0300

    Cosmetic

---
+++ lib/models/avatar/avatar_model.g.dart
@@ -0,0 +1,15 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'avatar_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_Avatar _$$_AvatarFromJson(Map<String, dynamic> json) => _$_Avatar(
+      file: json['file'] as String?,
+    );
+
+Map<String, dynamic> _$$_AvatarToJson(_$_Avatar instance) => <String, dynamic>{
+      'file': instance.file,
+    };
