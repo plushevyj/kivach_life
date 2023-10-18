b0201f253fa4dd08a737893d982b26455bbcca13
kirill <ischencko.kir@gmail.com>
2023-10-18 18:29:10 +0300

    Cosmetic

--- lib/pages/settings/profile_setting_page/profile_setting_page.dart
+++ lib/pages/settings/profile_setting_page/profile_setting_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter_android/webview_flutter_android.dart';
+import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
 
 import '../../../modules/authentication/repository/token_repository.dart';
 import '../../../modules/opening_app/controllers/configuration_of_app_controller.dart';
@@ -12,40 +14,66 @@ class ProfileSettingPage extends StatelessWidget {
   final configuration =
       Get.find<ConfigurationOfAppController>().configuration.value;
 
-  static PlatformWebViewControllerCreationParams params = (() {
-    if (GetPlatform.isIOS) {
-      return const PlatformWebViewControllerCreationParams();
-    } else {
-      // return WebViewPlatform.instance is WebKitWebViewPlatform
-      //     ? WebKitWebViewControllerCreationParams(
-      //         allowsInlineMediaPlayback: true,
-      //         mediaTypesRequiringUserAction: const {
-      //           PlaybackMediaTypes.audio,
-      //           PlaybackMediaTypes.video,
-      //         },
-      //       )
-      //     : const PlatformWebViewControllerCreationParams();
-      // return WebKitWebViewControllerCreationParams(
-      //   allowsInlineMediaPlayback: true,
-      //   mediaTypesRequiringUserAction: const {
-      //     PlaybackMediaTypes.audio,
-      //     PlaybackMediaTypes.video,
-      //   },
-      // );
-      return const PlatformWebViewControllerCreationParams();
-    }
-  })();
+  // static PlatformWebViewControllerCreationParams params = (() {
+  //
+  //
+  //
+  //
+  //
+  //   if (GetPlatform.isIOS) {
+  //     return const PlatformWebViewControllerCreationParams();
+  //   } else {
+  //     // return WebViewPlatform.instance is WebKitWebViewPlatform
+  //     //     ? WebKitWebViewControllerCreationParams(
+  //     //         allowsInlineMediaPlayback: true,
+  //     //         mediaTypesRequiringUserAction: const {
+  //     //           PlaybackMediaTypes.audio,
+  //     //           PlaybackMediaTypes.video,
+  //     //         },
+  //     //       )
+  //     //     : const PlatformWebViewControllerCreationParams();
+  //     // return WebKitWebViewControllerCreationParams(
+  //     //   allowsInlineMediaPlayback: true,
+  //     //   mediaTypesRequiringUserAction: const {
+  //     //     PlaybackMediaTypes.audio,
+  //     //     PlaybackMediaTypes.video,
+  //     //   },
+  //     // );
+  //     return const PlatformWebViewControllerCreationParams();
+  //   }
+  // })();
 
-  final webViewController = WebViewController.fromPlatformCreationParams(
-    params,
-    onPermissionRequest: (request) {
-      request.grant();
-    },
-  );
+  // final webViewController = WebViewController.fromPlatformCreationParams(
+  //   params,
+  //   onPermissionRequest: (request) {
+  //     request.grant();
+  //   },
+  // );
+
+  late final WebViewController webViewController;
 
   @override
   Widget build(BuildContext context) {
     Get.put(ProfileSettingsPageController());
+    PlatformWebViewControllerCreationParams params =
+        const PlatformWebViewControllerCreationParams();
+
+    if (WebViewPlatform.instance is WebKitWebViewPlatform) {
+      params = WebKitWebViewControllerCreationParams
+          .fromPlatformWebViewControllerCreationParams(
+        params,
+      );
+    } else if (WebViewPlatform.instance is AndroidWebViewPlatform) {
+      params = AndroidWebViewControllerCreationParams
+          .fromPlatformWebViewControllerCreationParams(
+        params,
+      );
+    }
+
+    webViewController = WebViewController.fromPlatformCreationParams(params,
+        onPermissionRequest: (request) {
+      print('request.types = ${request.types}');
+    });
     webViewController
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..setBackgroundColor(const Color(0x00000000))
