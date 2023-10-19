f5e3e654cf01580003653a03e82496f828a3fb22
kirill <ischencko.kir@gmail.com>
2023-10-19 13:54:14 +0300

    Cosmetic

--- lib/pages/settings/profile_setting_page/profile_setting_page.dart
+++ lib/pages/settings/profile_setting_page/profile_setting_page.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:webview_flutter_android/webview_flutter_android.dart';
 import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
@@ -7,6 +10,11 @@ import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
 import '../../../modules/authentication/repository/token_repository.dart';
 import '../../../modules/opening_app/controllers/configuration_of_app_controller.dart';
 import 'profile_settings_page_controller.dart';
+
+import 'package:image/image.dart' as image;
+import 'package:image_picker/image_picker.dart' as image_picker;
+import 'package:webview_flutter_android/webview_flutter_android.dart'
+    as webview_flutter_android;
 
 class ProfileSettingPage extends StatelessWidget {
   ProfileSettingPage({super.key});
@@ -63,17 +71,48 @@ class ProfileSettingPage extends StatelessWidget {
           .fromPlatformWebViewControllerCreationParams(
         params,
       );
+      webViewController = WebViewController.fromPlatformCreationParams(params,
+          onPermissionRequest: (request) {
+        print('request.types = ${request.types}');
+      });
     } else if (WebViewPlatform.instance is AndroidWebViewPlatform) {
-      params = AndroidWebViewControllerCreationParams
-          .fromPlatformWebViewControllerCreationParams(
-        params,
-      );
-    }
+      print(
+          'WebViewPlatform.instance is AndroidWebViewPlatform = ${WebViewPlatform.instance is AndroidWebViewPlatform}');
+      final androidWebViewController =
+          (WebViewPlatform.instance as AndroidWebViewPlatform)
+              .createPlatformWebViewController(params);
+      androidWebViewController
+        ..setMediaPlaybackRequiresUserGesture(true)
+        ..setOnShowFileSelector((params) async {
+          if (params.acceptTypes.any((type) => type == 'image/*')) {
+            final picker = image_picker.ImagePicker();
+            final photo = await picker.pickImage(
+                source: image_picker.ImageSource.gallery);
 
-    webViewController = WebViewController.fromPlatformCreationParams(params,
-        onPermissionRequest: (request) {
-      print('request.types = ${request.types}');
-    });
+            if (photo == null) {
+              return [];
+            }
+
+            final imageData = await photo.readAsBytes();
+            final decodedImage = image.decodeImage(imageData)!;
+            final scaledImage = image.copyResize(decodedImage, width: 500);
+            final jpg = image.encodeJpg(scaledImage, quality: 90);
+
+            final filePath = (await getTemporaryDirectory()).uri.resolve(
+                  './image_${DateTime.now().microsecondsSinceEpoch}.jpg',
+                );
+            final file = await File.fromUri(filePath).create(recursive: true);
+            await file.writeAsBytes(jpg, flush: true);
+
+            return [file.uri.toString()];
+          }
+
+          return [];
+        });
+      // params = androidWebViewController.params;
+      webViewController =
+          WebViewController.fromPlatform(androidWebViewController);
+    }
     webViewController
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..setBackgroundColor(const Color(0x00000000))
